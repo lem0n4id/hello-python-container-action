47fc97da3eadb4ee549eceedf4d447b9f1638643
Lenin <61219881+lem0n4id@users.noreply.github.com>
2021-11-21 22:54:25 +0530

    feat: Change action and example workflows to just print hello world (#2)
    * Removed input myInput and output myOutput from the action

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
-
+# import os
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+#     my_input = os.environ["INPUT_MYINPUT"] env variable
 
-    my_output = f"Hello {my_input}"
-
-    # "::" is a special syntax to run the workflow commands.
-    # https://docs.github.com/en/actions/learn-github-actions/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
-    print(f"::set-output name=myOutput::{my_output}")
+    print("hello world!")
 
 
 if __name__ == "__main__":
